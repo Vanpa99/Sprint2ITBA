3b521ee0fbd827020ce9999a90efbd49fcb4d45f
Vanpa99 <mativanpa99@gmail.com>
2024-09-07 18:54:48 -0300

    Enrutamiento

---
+++ Sprint2/src/components/Pagar.jsx
@@ -0,0 +1,10 @@
+function Pagar() {
+    return ( 
+        <>
+            <h2>Metodos de Pago</h2>
+            <div id="cont_log_transferencias"></div>
+        </>
+     );
+}
+
+export default Pagar;
---
+++ Sprint2/src/components/Prestamos.jsx
@@ -0,0 +1,10 @@
+function Prestamos() {
+    return (
+        <div>
+            <h2>Calculadora De Prestamos</h2>
+            <div id="cont_log_prestamos"></div>
+        </div>
+    );
+}
+
+export default Prestamos;
